0c42a52883b78a2dc7326bf9f3e39846dda840e2
DESKTOP-MTKK7HN\Nabeel <mehedisaurav@gmail.com>
2017-03-21 12:16:56 +0600

    Simple CRUD operation by ADO.NET - 1

---
+++ SchoolManagement.ConsoleApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using SchoolManagement.Model;
+using SchoolManagement.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentManager manager = new StudentManager();
+            
+           Student student = manager.GetStudent(1);
+            Console.WriteLine("Name:"+student.Name +"\nAddress : "+student.Address+"\nPhone : "+student.Phone);
+            Console.Read();
+        }
+    }
+}
---
+++ SchoolManagement.Manager/StudentManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolManagement.Model;
+
+namespace SchoolManagement.Manager
+{
+   public class StudentManager
+   {
+        SchoolManagementDbEntities Db;
+       public StudentManager()
+       {
+           Db = new SchoolManagementDbEntities();
+       }
+
+       public void Add(Student student)
+       {
+           DbSet<Student> students = Db.Students;
+           Db.Students.Add(student);
+           Db.SaveChanges();
+       }
+
+       public void Delete(int id)
+       {
+           Student students = Db.Students.FirstOrDefault(student => student.Id == id);
+           if (students != null)
+           {
+                Db.Students.Remove(students);
+            }
+           Db.SaveChanges();
+       }
+
+       public Student GetStudent(int id)
+       {
+           Student s = Db.Students.FirstOrDefault(std => std.Id == id);
+           ;
+           return s;
+
+
+       }
+   }
+}
